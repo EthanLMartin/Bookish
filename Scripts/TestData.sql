6e08a06ec4845e132bcb5c5a40e308c1560a0eb2
EthanLMartin <Ethan.Martin@Softwire.com>
2019-09-11 15:24:15 +0100

    moving files

--- Scripts/TestData.sql
+++ Scripts/TestData.sql
@@ -6,29 +6,23 @@ DELETE FROM Books;
 DELETE FROM Users;
 
 -- Default Test values
-INSERT INTO Users
-VALUES ('person', 'Fred', 'password');
+INSERT INTO Users VALUES
+('person', 'Fred', 'password'),
+('person2', 'Fréd', 'passwórd');
 
-INSERT INTO Users
-VALUES ('person2', 'Fréd', 'passwórd');
+INSERT INTO Books VALUES 
+('9780132350884', 'Clean Code', 'Robert C. Martin'),
+('9780590353403', 'Harry Potter And The Sorcerers Stone', 'J. K. Rowling'),
+('9780439064866', 'Harry Potter and the Chamber of Secrets', 'J. K. Rowling');
 
-INSERT INTO Books
-VALUES ('9780132350884', 'Clean Code', 'Robert C. Martin')
+INSERT INTO BookCopies VALUES
+('1', '9780132350884', 0),
+('2', '9780132350884', 1),
+('3', '9780132350884', 1);
 
-INSERT INTO BookCopies
-VALUES ('1', '9780132350884', 0);
-
-INSERT INTO BookCopies
-VALUES ('2', '9780132350884', 1);
-
-INSERT INTO BookCopies
-VALUES ('3', '9780132350884', 1);
-
-INSERT INTO Loans
-VALUES ('2', 'person', '2019-09-12', 0);
-
-INSERT INTO Loans
-VALUES ('3', 'person2', '2019-09-10', 0);
+INSERT INTO Loans VALUES 
+('2', 'person', '2019-09-12', 0),
+('3', 'person2', '2019-09-10', 0);
 
 -- Test query
 SELECT *
